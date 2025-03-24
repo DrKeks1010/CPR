61889338e0fcecbd60ec0edc72423d9b24846a5b
DrKeks1010 <jonas.putz@icloud.com>
2025-03-24 21:36:27 +0100

    Fixes C formatting

--- src/main.c
+++ src/main.c
@@ -10,46 +10,45 @@
 int main(void) 
 {
   // Init glfw
-   glfwInit();
-   // Set glfw hints
-   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwInit();
+  // Set glfw hints
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-   // Create glfw window
-   GLFWwindow* window =
-       glfwCreateWindow(_WINDOW_WIDTH, _WINDOW_HEIGHT, _WINDOW_NAME, NULL, NULL);
-   if (window == NULL) 
-   {
-     perror(NULL);
-     fputs("Failed to create GLFW window!\n", stderr);
-     glfwTerminate();
-     exit(-1);
-   }
-   // Get window context
-   glfwMakeContextCurrent(window);
+  // Create glfw window
+  GLFWwindow* window = glfwCreateWindow(_WINDOW_WIDTH, _WINDOW_HEIGHT, _WINDOW_NAME, NULL, NULL);
+  if (window == NULL) 
+  {
+    perror(NULL);
+    fputs("Failed to create GLFW window!\n", stderr);
+    glfwTerminate();
+    exit(-1);
+  }
+  // Get window context
+  glfwMakeContextCurrent(window);
 
-   // Load necessary opengl methods
-   gladLoadGL();
+  // Load necessary opengl methods
+  gladLoadGL();
 
-   // Set the opengl viewport
-   glViewport(0, 0, _WINDOW_WIDTH, _WINDOW_HEIGHT);
+  // Set the opengl viewport
+  glViewport(0, 0, _WINDOW_WIDTH, _WINDOW_HEIGHT);
 
-   // Main window loop
-   while (!glfwWindowShouldClose(window)) 
-   {
-     glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-     glClear(GL_COLOR_BUFFER_BIT);
+  // Main window loop
+  while (!glfwWindowShouldClose(window)) 
+  {
+    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-     glfwSwapBuffers(window);
+    glfwSwapBuffers(window);
 
-     // Poll GLFW events
-     glfwPollEvents();
-   }
+    // Poll GLFW events
+    glfwPollEvents();
+  }
 
-   // Close resources
-   glfwDestroyWindow(window);
-   glfwTerminate();
+  // Close resources
+  glfwDestroyWindow(window);
+  glfwTerminate();
 
   printf("Project exited without any errors!\n");
   return 0;
